ddb1f5afce93057cdf38217ce14f091947600a98
Jun Wang <junwang2003@gmail.com>
2020-03-01 12:28:51 -0600

    update save auth script

--- cypress/integration/Novologix/TC_01_viewauthdetails.js
+++ cypress/integration/Novologix/TC_01_viewauthdetails.js
@@ -3,16 +3,29 @@ describe('View auth details', function () {
     Cypress.config('pageLoadTimeout', 100000)
 
     before(function () {
-       cy.NLXLogin()
+        cy.NLXLogin()
     })
 
     it('View Auth Details', function () {
         cy.get('[row-index="4"] > .ag-cell-last-left-pinned > a').click()
         cy.get('.modal-headerAlert > div').should('contain', 'Your Authorization')
-        
+
         cy.url().should('include', 'authorization/show')
-        cy.wait()
-        cy.get('[formarrayname="requestedcycles"] > auth-regimen-cycle > [style="border : 1px solid #ddd; border-radius : 4px; padding: 5px;"] > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(2) > auth-regimen-di > fieldset > :nth-child(3) > .col-md-12 > :nth-child(2) > auth-regimen-di-dose > :nth-child(1) > [style="width:8%"] > nlx-custom-input > div.ng-untouched > .nlx-control').should('contain', '840')
+
+        cy.get('#btnCopy', { timeout: 30000 }).should('be.visible').click()
+        cy.get('#modalid > .modal-content > .modal-footer > .nlx-button-positive').should('be.visible').click()
+        cy.get('.injectableSection > form.ng-untouched > :nth-child(1) > .form-group > .col-md-7 > nlx-calendar > div.ng-untouched > [style="white-space:nowrap;"] > span.nlx-label', { timeout: 30000 }).click()
+        cy.get('.injectableSection > form.ng-pristine > :nth-child(1) > .form-group > .col-md-7 > nlx-calendar > div.ng-pristine > .ui-dropdown-panel > :nth-child(1) > div > :nth-child(2)').click()
+        cy.get('.col-md-7 > nlx-calendar > .ng-invalid > .ui-dropdown-panel > [style="width:100%; height:25px; padding-left:5px; padding-right:5px; padding-top:2px; margin-bottom:10px;"] > :nth-child(2)').click()
+        cy.get('.col-md-7 > div > .ui-inputtext').select('Normal')
+        cy.get(':nth-child(3) > .col-md-12 > [style="display:inline-block;"] > :nth-child(1) > .ui-inputtext').select('Office')
+        cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-control').type('Perjeta')
+        cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-label > .fa').click()
+        //cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-control').select()
+        cy.get('[style="line-height:20px;width:12%;text-align:left;"]').click()
+        cy.get('#btnSave').should('be.visible').click()
+        cy.get('#btnSubmitAuthorization').should('be.visible')
+
     })
 
     // it('Mark-unmark as favorite', function () {
