1ac32a824fafd584124ca4c12c657ae6b09fb2f1
Jun Wang <junwang@aligncaresolutions.com>
2020-03-01 18:52:46 -0600

    update submit auth for regimen

--- cypress/integration/Novologix/TC_01_viewauthdetails.spec.js
+++ cypress/integration/Novologix/TC_01_viewauthdetails.spec.js
@@ -8,8 +8,8 @@ describe('Copy auth from existing one', function () {
 
     it('View Auth Details - Copy auth - Save auth', () => {
         cy.get('[style="width: 90px; left: 150px;"] > .ag-floating-filter-body > .ag-input-wrapper > .ag-floating-filter-input', { timeout: 30000 }).type('431478{enter}')
+        //cy.wait(1000)
         cy.get('.ag-cell-last-left-pinned > a').should('contain', '431478').first().click()
-
         cy.get('.modal-headerAlert > div').should('contain', 'Your Authorization')
         cy.url().should('include', 'authorization/show')
         cy.get('#btnCopy', { timeout: 30000 }).should('be.visible').click()
@@ -20,14 +20,22 @@ describe('Copy auth from existing one', function () {
         cy.get('.col-md-7 > nlx-calendar > .ng-invalid > .ui-dropdown-panel > [style="width:100%; height:25px; padding-left:5px; padding-right:5px; padding-top:2px; margin-bottom:10px;"] > :nth-child(2)').click()
         cy.get('.col-md-7 > div > .ui-inputtext').select('Normal')
         cy.get(':nth-child(3) > .col-md-12 > [style="display:inline-block;"] > :nth-child(1) > .ui-inputtext').select('Office')
-        cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-control').type('Perjeta')
+        cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-control').clear().type('Perjeta')
         cy.get('.src-panelShadow > div.ng-untouched > [style="white-space:nowrap;"] > .nlx-label > .fa').click()
 
         cy.get('[style="line-height:20px;width:12%;text-align:left;"]').click()
         cy.get('#btnSave', { timeout: 30000 }).should('be.visible').should('not.be.disabled').click()
         cy.get('#btnSubmitAuthorization', { timeout: 30000 }).should('be.visible')
-
-
+        cy.get('#btnSubmitAuthorization').click()
+        
+        cy.get('stages-select > .row > .selectPadding > .ui-inputtext',{timeout:30000}).select('Stage II')
+        cy.get('indications-select > .row > .selectPadding > .ui-inputtext').select('Adjuvant')
+       
+        cy.get('.ui-multiselect-label').select(['HER2 Positive'])
+        
+        cy.get('performance-select > .row > .selectPadding > .ui-inputtext').select('eCOG-2')
+        cy.get(':nth-child(2) > .col-md-12 > .ng-dirty > .ng-untouched').check()
+        cy.get(':nth-child(3) > .col-md-12 > .ng-dirty > .ng-untouched').check()
     })
 
 })
